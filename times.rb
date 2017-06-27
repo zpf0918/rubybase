41a7a1fd9f785cfc7203ae5994681dabe4419ae0
zpf0918 <zhanggongzifei@163.com>
2017-06-27 17:15:14 +0800

    add times.rb

--- times.rb
+++ times.rb
@@ -1,3 +1,20 @@
-7.times do
-  puts "哈哈"
+puts"演示1"
+5.times do
+  puts "ruby on rails"
+end
+
+puts"演示2"
+5.times {
+  puts "ruby on rails "
+}
+
+puts"演示3"
+
+5.times do |i|
+  puts "第#{i}次的循环"
+end
+
+puts"演示4"
+5.times do |i|
+  puts "第#{i+1}次的循环"
 end
