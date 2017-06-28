3fe60c47088ec108e7540908436710a2e79be003
zpf0918 <zhanggongzifei@163.com>
2017-06-28 13:14:17 +0800

    exercise prints and puts

--- helloruby.rb
+++ helloruby.rb
@@ -1,1 +1,13 @@
-print("Hello, Ruby.\n")
+print(" Hello, \n RUBY!\n") #\n是换行符,为转义,()可以省略
+print("hello, \"ruby\".\n ")
+print("Hello, \"Ruby\",\n ")# 想输入含有特殊符号的字符串要用到“\”
+print("hello, \\ruby.\n")
+print('Hello, \n\Ruby\n') #单引号内程序会原封不动地输出
+puts "  单引号的例外情况如下"
+print('Hello, \\ \'Ruby\'. ')
+
+
+print "Hello, "
+print "Ruby"
+print "."
+print "\n"
