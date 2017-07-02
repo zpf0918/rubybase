d5b6fa9ff354ba58000c03a2163cdf687ce039d2
zpf0918 <zhanggongzifei@163.com>
2017-07-02 17:42:06 +0800

    implement 15-1

---
+++ section-15/15-1.rb
@@ -0,0 +1,11 @@
+wday = {
+  "sunday"     => "星期天",
+  "monday"     => "星期一",
+  "tuesday"    => "星期二",
+  "wedensday"  => "星期三",
+  "thursday"   => "星期四",
+  "friday"     => "星期五",
+  "saturday"   => "星期六",
+}
+
+p wday
