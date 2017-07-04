1d73c1a286151cf624b1e7910906e3a94e49eadf
zpf0918 <zhanggongzifei@163.com>
2017-07-04 17:11:22 +0800

    implement 19-3.rb

---
+++ section-19/19-3.rb
@@ -0,0 +1,12 @@
+str = "喜"
+
+encoding = [Encoding::GB18030, Encoding::GBK]
+encoding.each do |enc|
+  begin
+    print "将str转换为#{enc}。=>"
+    puts "结果: %p" % [str.encode(enc)]
+  rescue => ex
+    p ex
+  end
+  puts 
+end
