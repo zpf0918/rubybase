7567787ff92af08affe1e0007b4330ff9ab77456
zpf0918 <zhanggongzifei@163.com>
2017-09-06 11:05:44 +0800

    add method_missing.rb

---
+++ methods/method_missing.rb
@@ -0,0 +1,16 @@
+class Computer
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
+
+  def method_missing(name)
+    super if !@data_source.respond_to?("get_#{name}_info")
+    info = @data_source.send("get_#{name}_info", @id)
+    price = @data_source.send("get_#{name}_price", @id)
+    result = "#{name.capitalize}: #{info} ($#{price})"
+    return "* #{result}" if price > 100
+    result
+  end
+
+end
